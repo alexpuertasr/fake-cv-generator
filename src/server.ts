dada3e9c89458a77060c2491cec725806aa7e115
Alex Puertas <alexpuertasr@gmail.com>
2022-05-10 19:12:20 +0200

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 
 import { getRoutes } from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use('/api', getRoutes());
 
 const PORT = process.env.PORT ?? 3000;
