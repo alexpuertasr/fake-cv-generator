6fc2e16a7fde93282743697ff19fb64c4abbf32e
Alex Puertas <alexpuertasr@gmail.com>
2022-05-11 20:40:07 +0200

    fix: move fonts on root

--- src/utils/getCandidateDOC.ts
+++ src/utils/getCandidateDOC.ts
@@ -5,10 +5,10 @@ import { Candidate } from '../types/types';
 
 const fonts = {
   Roboto: {
-    normal: path.join(__dirname, '../fonts/Roboto-Regular.ttf'),
-    bold: path.join(__dirname, '../fonts/Roboto-Medium.ttf'),
-    italics: path.join(__dirname, '../fonts/Roboto-Italic.ttf'),
-    bolditalics: path.join(__dirname, '../fonts/Roboto-MediumItalic.ttf'),
+    normal: path.join(__dirname, '../../fonts/Roboto-Regular.ttf'),
+    bold: path.join(__dirname, '../../fonts/Roboto-Medium.ttf'),
+    italics: path.join(__dirname, '../../fonts/Roboto-Italic.ttf'),
+    bolditalics: path.join(__dirname, '../../fonts/Roboto-MediumItalic.ttf'),
   },
 };
 
